24e370183c77116485371a6dad2f28622df67fbd
Ethan Lee <ethantlee21@gmail.com>
2026-03-10 23:12:59 -0400

    feat(grafana): Enable UI updates for provisioned dashboards

--- grafana/entrypoint.sh
+++ grafana/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 
+# Map PORT to Grafana's native env var
 export GF_SERVER_HTTP_PORT="${PORT:-3000}"
 
 exec /run.sh "$@"
--- prometheus/entrypoint.sh
+++ prometheus/entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 set -e
 
+# Substitute env vars into the config template (Prometheus lacks native env expansion)
 sed -e "s|\${OTEL_COLLECTOR_HOST}|${OTEL_COLLECTOR_HOST}|g" \
     -e "s|\${OTEL_COLLECTOR_PORT}|${OTEL_COLLECTOR_PORT}|g" \
     -e "s|\${SCRAPE_INTERVAL}|${SCRAPE_INTERVAL}|g" \
     -e "s|\${EVALUATION_INTERVAL}|${EVALUATION_INTERVAL}|g" \
     /etc/prometheus/config.yaml.tmpl > /etc/prometheus/config.yaml
 
+# --web.listen-address and --storage.tsdb.path are CLI-only in Prometheus (not configurable via config file)
 exec prometheus \
   --config.file=/etc/prometheus/config.yaml \
   --storage.tsdb.path=/prometheus \
