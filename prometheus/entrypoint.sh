9ff2d5f398f1a587da9ccaad878a6b16860368c0
Ethan Lee <ethantlee21@gmail.com>
2026-03-18 16:35:44 -0400

    feat(observability): integrate Alloy and update Grafana dashboards

--- prometheus/entrypoint.sh
+++ prometheus/entrypoint.sh
@@ -2,9 +2,7 @@
 set -e
 
 # Substitute env vars into the config template (Prometheus lacks native env expansion)
-sed -e "s|\${OTEL_COLLECTOR_HOST}|${OTEL_COLLECTOR_HOST}|g" \
-    -e "s|\${OTEL_COLLECTOR_PORT}|${OTEL_COLLECTOR_PORT}|g" \
-    -e "s|\${SCRAPE_INTERVAL}|${SCRAPE_INTERVAL}|g" \
+sed -e "s|\${SCRAPE_INTERVAL}|${SCRAPE_INTERVAL}|g" \
     -e "s|\${EVALUATION_INTERVAL}|${EVALUATION_INTERVAL}|g" \
     /etc/prometheus/config.yaml.tmpl > /etc/prometheus/config.yaml
 
@@ -12,4 +10,5 @@ sed -e "s|\${OTEL_COLLECTOR_HOST}|${OTEL_COLLECTOR_HOST}|g" \
 exec prometheus \
   --config.file=/etc/prometheus/config.yaml \
   --storage.tsdb.path=/prometheus \
+  --web.enable-remote-write-receiver \
   --web.listen-address="0.0.0.0:${PORT}"
